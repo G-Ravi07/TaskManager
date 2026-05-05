3fd92befe0ff9b62134016342c3839ebeacee1ff
Ravi G <raviindra2002@gmail.com>
2026-05-05 19:32:36 +0530

    restore templates login

--- users/views.py
+++ users/views.py
@@ -21,8 +21,9 @@ def signup_view(request):
     return render(request, "signup.html")
 
 
+
 def login_view(request):
-    return HttpResponse("LOGIN WORKING")
+    return render(request, "login.html")
 
 def logout_view(request):
     logout(request)
